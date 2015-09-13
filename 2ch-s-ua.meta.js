a6042af818ff0e0617b6de18bbff78cfe5f41179
AlsaAsksMiley <AlsaAsksMiley@users.noreply.github.com>
2015-09-14 01:42:07 +0300

    Fix updateURL

--- 2ch-s-ua.meta.js
+++ 2ch-s-ua.meta.js
@@ -7,6 +7,6 @@
 // @include      /^https?:\/\/2ch\.(hk|pm|re|tf|wf|yt)\/s/
 // @version     v1.5.2
 // @downloadURL https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.user.js
-// @updateURL   https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.user.js
+// @updateURL   https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.meta.js
 // @grant       none
 // ==/UserScript==
