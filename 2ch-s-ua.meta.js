a0760bace45421859c6f5036d3856886f022997b
AlsaAsksMiley <AlsaAsksMiley@users.noreply.github.com>
2015-11-16 01:41:00 +0300

    Changed UCBrowser icon

--- 2ch-s-ua.meta.js
+++ 2ch-s-ua.meta.js
@@ -8,7 +8,7 @@
 // @include      http://2ch.pm/s/*
 // @include      https://2ch.hk/s/*
 // @include      https://2ch.pm/s/*
-// @version     v1.6.3
+// @version     v1.6.4
 // @downloadURL https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.user.js
 // @updateURL   https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.meta.js
 // @grant       none
