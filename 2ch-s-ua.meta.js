329b34da13dd38f184bb8aecc08974f5bec61f2d
AlsaAsksMiley <AlsaAsksMiley@users.noreply.github.com>
2015-11-16 00:54:55 +0300

    Fixed include rules

--- 2ch-s-ua.meta.js
+++ 2ch-s-ua.meta.js
@@ -4,8 +4,11 @@
 // @namespace   2ch_hk
 // @description    Adds OS and browser flags to messages on 2ch.hk
 // @description:ru Добавляет флаги ОС и браузера в сообщения на 2ch.hk
-// @include      /^https?:\/\/2ch\.(hk|pm|re|tf|wf|yt)\/s/
-// @version     v1.6.1
+// @include      http://2ch.hk/s/
+// @include      http://2ch.pm/s/
+// @include      https://2ch.hk/s/
+// @include      https://2ch.pm/s/
+// @version     v1.6.2
 // @downloadURL https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.user.js
 // @updateURL   https://github.com/AlsaAsksMiley/2ch-ua-flagger/raw/master/2ch-s-ua.meta.js
 // @grant       none
